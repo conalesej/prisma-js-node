72bcce93cbc7b5f62c3ce0d48e9fbce83b35ee47
Ephraim Jan Conales <conalesej@gmail.com>
2023-10-10 20:18:55 +0800

    Script - connected prisma to mysql

--- script.ts
+++ script.ts
@@ -1,23 +1,16 @@
-// import { PrismaClient } from "@prisma/client/edge";
-// const prisma = new PrismaClient({
-//   datasources: {
-//     db: {
-//       url: "prisma://root:admin1234@localhost:3306/prismadb",
-//     },
-//   },
-// }); 
-
-import { PrismaClient } from '@prisma/client/edge'
-const prisma = new PrismaClient()
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 
 const main = async () => {
   // ..write your Prisma Client queries ehre
 
-  const _newUser = { name: "Ej" };
+  const _newUser = { name: "Mariel" };
 
-  const user = await prisma.user.create({ data: { ..._newUser } });
+  // const user = await prisma.user.create({ data: { ..._newUser } });
 
-  console.log({ user });
+  const users = await prisma.user.findMany();
+
+  console.log( users );
 };
 
 main()
