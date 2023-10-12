088005bf5b1a99fd6aa2c54a8b425cc9f1760d48
Ephraim Jan Conales <conalesej@gmail.com>
2023-10-12 18:46:54 +0800

    Script - Add create query with select and include

--- script.ts
+++ script.ts
@@ -7,15 +7,32 @@ const main = async () => {
   // const user = await prisma.user.create({ data: { ..._newUser } });
   // const users = await prisma.user.findMany();
 
+  await prisma.user.deleteMany();
   const newUser = await prisma.user.create({
     data: {
       name: "Ej Conales",
       age: 24,
       email: "conalesej@gmail.com",
+      userPreference: {
+        create: {
+          emailUpdates: true,
+        },
+      },
     },
+    // select: {
+    //   name: true,
+    //   userPreference: {
+    //     select: {
+    //       emailUpdates: true,
+    //     },
+    //   },
+    // },
+    // include: {     / When including a child / foreign entity
+    //   userPreference: true,
+    // },
   });
 
-  
+  console.log(newUser);
 };
 
 main()
