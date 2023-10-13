9a6fd0b1384967de715afd873ced3e0450d9c217
Ephraim Jan Conales <conalesej@gmail.com>
2023-10-13 17:26:06 +0800

    Script - add findUnique

--- script.ts
+++ script.ts
@@ -8,38 +8,48 @@ const main = async () => {
   // const users = await prisma.user.findMany();
 
   await prisma.user.deleteMany();
-  const newUser = await prisma.user.createMany({
-    data: [
-      {
-        name: "Ej Conales",
+  // const newUser = await prisma.user.createMany({
+  //   data: [
+  //     {
+  //       name: "Ej Conales",
+  //       age: 24,
+  //       email: "conalesej@gmail.com",
+  //     },
+  //     {
+  //       name: "Elaine Mizpa",
+  //       age: 29,
+  //       email: "newEmail@gmail.com",
+  //       // userPreference: {
+  //       //   create: {
+  //       //     emailUpdates: true,
+  //       //   },
+  //       // },
+  //     },
+  //   ],
+  //   // select: {
+  //   //   name: true,
+  //   //   userPreference: {
+  //   //     select: {
+  //   //       emailUpdates: true,
+  //   //     },
+  //   //   },
+  //   // },
+  //   // include: {     / When including a child / foreign entity
+  //   //   userPreference: true,
+  //   // },
+  // });
+
+  const user = await prisma.user.findUnique({
+    where: {
+      email: "conalesej@gmail.com",
+      age_name: {
         age: 24,
-        email: "conalesej@gmail.com",
+        name: "Ej",
       },
-      {
-        name: "Elaine Mizpa",
-        age: 29,
-        email: "newEmail@gmail.com",
-        // userPreference: {
-        //   create: {
-        //     emailUpdates: true,
-        //   },
-        // },
-      },
-    ],
-    // select: {
-    //   name: true,
-    //   userPreference: {
-    //     select: {
-    //       emailUpdates: true,
-    //     },
-    //   },
-    // },
-    // include: {     / When including a child / foreign entity
-    //   userPreference: true,
-    // },
+    },
   });
 
-  console.log(newUser.count);
+  console.log(user);
 };
 
 main()
