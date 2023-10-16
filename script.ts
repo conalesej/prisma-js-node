020419765921173fda03804824ee2d1388fce315
Ephraim Jan Conales <conalesej@gmail.com>
2023-10-16 23:10:17 +0800

    Script - update email

--- script.ts
+++ script.ts
@@ -39,66 +39,73 @@ const main = async () => {
   //   // },
   // });
 
-  await prisma.user.createMany({
-    data: [
-      {
-        name: "Ej",
-        age: 24,
-        email: "conalesej@gmail.com",
-      },
-      {
-        name: "Sally",
-        age: 29,
-        email: "Hello.com@gmail.com.ph",
-        role: "BASIC",
-      },
-    ],
+  // await prisma.user.createMany({
+  //   data: [
+  //     {
+  //       name: "Ej",
+  //       age: 24,
+  //       email: "conalesej@gmail.com",
+  //     },
+  //     {
+  //       name: "Sally",
+  //       age: 29,
+  //       email: "Hello.com@gmail.com.ph",
+  //       role: "BASIC",
+  //     },
+  //   ],
+  // });
+
+  // const user = await prisma.user.findMany({
+  //   where: {
+  //     // writtenPosts: {
+  //     //   every: {
+  //     //     title: "Test",
+  //     //   },
+  //     // },
+
+  //     // userPreference: {     // one to one
+  //     //   emailUpdates: true,
+  //     // },
+
+  //     // AND: [ // OR / NOT = negates all the shit
+  //     //   {
+  //     //     email: {
+  //     //       endsWith: "@gmail.com",
+  //     //     },
+  //     //   },
+  //     //   {
+  //     //     age: {
+  //     //       gte: 20,
+  //     //     },
+  //     //   },
+  //     // ],
+
+  //     // email: {
+  //     //   // contains: "@gmail.com", // or endsWith / starts with
+  //     //   // equals: "conalesej@gmail.com",
+  //     //   // not: "conalesej@gmail.com",
+  //     //   in: ["Ej", "Elaine"], // or //Not In
+  //     // },
+  //     // age: {
+  //     //   lte: 20, // Less than or equal to 20
+  //     // },
+  //   },
+  //   distinct: ["name", "age"], // Return all the user with an email that contains @gmail.com and with a unique name and age
+  //   take: 2, // paginated get 2
+  //   skip: 1, // skipp the first item
+  //   orderBy: {
+  //     age: "asc",
+  //   },
+  // });
+
+  const user = await prisma.user.update({
+    data: {
+      email: "testUpdate@gmail.com",
+    },
+    where: { email: "conalesej@gmai.com" },
   });
 
-  const user = await prisma.user.findMany({
-    where: {
-      writtenPosts: {
-        every: {
-          title: "Test",
-        },
-      },
-
-      // userPreference: {     // one to one
-      //   emailUpdates: true,
-      // },
-
-      // AND: [ // OR / NOT = negates all the shit
-      //   {
-      //     email: {
-      //       endsWith: "@gmail.com",
-      //     },
-      //   },
-      //   {
-      //     age: {
-      //       gte: 20,
-      //     },
-      //   },
-      // ],
-
-      // email: {
-      //   // contains: "@gmail.com", // or endsWith / starts with
-      //   // equals: "conalesej@gmail.com",
-      //   // not: "conalesej@gmail.com",
-      //   in: ["Ej", "Elaine"], // or //Not In
-      // },
-      // age: {
-      //   lte: 20, // Less than or equal to 20
-      // },
-    },
-    distinct: ["name", "age"], // Return all the user with an email that contains @gmail.com and with a unique name and age
-    take: 2, // paginated get 2
-    skip: 1, // skipp the first item
-    orderBy: {
-      age: "asc",
-    },
-  });
-
-  console.log(user.length);
+  console.log(user);
 };
 
 main()
